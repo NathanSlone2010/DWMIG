9d949027090b0b4bf47e1f0925f0f57dc5fde5df
Nathan Slone <NathanSlone2010@icloud.com>
2026-02-11 18:45:37 +0000

    Update V-05.py

--- CORE/V-05.py
+++ CORE/V-05.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime
 
-
+time.sleep(1); print("PROGRAM STARTING.... [4 SECONDS]")
 time.sleep(1); print("PROGRAM STARTING... [3 SECONDS]")
 time.sleep(1); print("PROGRAM STARTING.. [2 SECONDS]")
 time.sleep(1); print("PROGRAM STARTING. [1 SECONDS]")
@@ -9,7 +9,7 @@ time.sleep(0.5); print("PROGRAM START")
 #A countdown to start the program. Remove it if you want.
 
 print("\n Darknet & Market Index / Guide")
-print("Version O-05.19.10")
+print("Version O-05.20.10")
 #Introduction
 
 
@@ -34,6 +34,7 @@ while True:
     document = input("\n Input: ")
     if document == "exit":
         print("Program will end. You cannot revert the command.")
+        time.sleep(1); print("5 SECOND[S] LEFT.")
         time.sleep(1); print("4 SECOND[S] LEFT.")
         time.sleep(1); print("3 SECOND[S] LEFT.")
         time.sleep(1); print("2 SECOND[S] LEFT.")
@@ -60,7 +61,7 @@ while True:
         print("Version: Official Release")
 
     elif document == "h":
-        print("Version 00.00.00: added the introduction | Version 00.01.00: added the core options | Version 00.01li.01: fixed the options selection | 01.01.01: added the 'not in' function | 01.01.02: fixed some typos | 02.01.02: finishes the core options of the program. It is now ready to be released to the public. Updates of course will still take place | 02.02.01: changed program name from 'Dark Web & Market Index / Guide' to 'Darknet & Market Index / Guide' as it is better fitting | 02.03.02: Added a donation link, to further fund projects and machines I use to make the projects | 02.04.02: Added a 'developer' section | 02.05.02: Added an add-on for developers | 02.05.03: Added a clear input section, so users know what to do | 03.05.03: Added the loop mechanism | 03.06.03: Forgot to remove the placeholder for Version History, sorry | 03.06.03: I am never doing placeholders again | 3.06.04: Fixed some typos. Should be clean for now | 3.06.05: Changed a few security ratings based off research | 3.06.06: Fixed some issues I accidentally created | 3.07.06: Removed the redundant 'not in' statement | 3.07.07: Added version shorthand next to the version-numbers | 3.07.08: Fixed some typos | 4.07.08: Program officially launched. First number changed to align. Will be going by 'V-O<>, O for OFFICIAL | 4.08.08: Added a largest donation amount section and changes the bulletin list from '-' to '#' | 4.09.08: Added two more options, Recommendations and Developer Recommendations | 4.10.08: made input simple | 4.10.08: Made some changes in donator area and enter-name-area | 4.11.08: Changed 'Dark Net' to 'Darknet' to be linguistically accurate | 4.11.10: Changed Dark Net to Darknet inside the printed text itself | 4.12.10: Added a Resources [rs] page to the selection and fixed the versioning issue in the History [h] page | 4.13.10: Removed the Security page from the program for easier maintenance, quicker updating, and easier user-usage | 4.14.10: Changed things in thye Archetypes page | 4.15.10: Added a 'news' page | 4.16.10: Added a mechanic that allows multiple variants of 'DEV' name | 4.17.10: Reverted mechanic back to previous as it did not work | 4.18.10: Removed donations and news page | 5.18.10: Added a countdown for the exit command and adde the Y/M/D time | 5.19.10: Added a timer to the beginning of the program. Tweaked end-program timer.")
+        print("Version 00.00.00: added the introduction | Version 00.01.00: added the core options | Version 00.01li.01: fixed the options selection | 01.01.01: added the 'not in' function | 01.01.02: fixed some typos | 02.01.02: finishes the core options of the program. It is now ready to be released to the public. Updates of course will still take place | 02.02.01: changed program name from 'Dark Web & Market Index / Guide' to 'Darknet & Market Index / Guide' as it is better fitting | 02.03.02: Added a donation link, to further fund projects and machines I use to make the projects | 02.04.02: Added a 'developer' section | 02.05.02: Added an add-on for developers | 02.05.03: Added a clear input section, so users know what to do | 03.05.03: Added the loop mechanism | 03.06.03: Forgot to remove the placeholder for Version History, sorry | 03.06.03: I am never doing placeholders again | 3.06.04: Fixed some typos. Should be clean for now | 3.06.05: Changed a few security ratings based off research | 3.06.06: Fixed some issues I accidentally created | 3.07.06: Removed the redundant 'not in' statement | 3.07.07: Added version shorthand next to the version-numbers | 3.07.08: Fixed some typos | 4.07.08: Program officially launched. First number changed to align. Will be going by 'V-O<>, O for OFFICIAL | 4.08.08: Added a largest donation amount section and changes the bulletin list from '-' to '#' | 4.09.08: Added two more options, Recommendations and Developer Recommendations | 4.10.08: made input simple | 4.10.08: Made some changes in donator area and enter-name-area | 4.11.08: Changed 'Dark Net' to 'Darknet' to be linguistically accurate | 4.11.10: Changed Dark Net to Darknet inside the printed text itself | 4.12.10: Added a Resources [rs] page to the selection and fixed the versioning issue in the History [h] page | 4.13.10: Removed the Security page from the program for easier maintenance, quicker updating, and easier user-usage | 4.14.10: Changed things in thye Archetypes page | 4.15.10: Added a 'news' page | 4.16.10: Added a mechanic that allows multiple variants of 'DEV' name | 4.17.10: Reverted mechanic back to previous as it did not work | 4.18.10: Removed donations and news page | 5.18.10: Added a countdown for the exit command and adde the Y/M/D time | 5.19.10: Added a timer to the beginning of the program. Tweaked end-program timer | 5.20/10: Chaged the countdown timer to be a second longer.")
 
     elif document == "de":
         print("\n Warning: If you are a developer, please use the term 'DEV' as your name | All work done by you will be credited to you on GitHub.")
