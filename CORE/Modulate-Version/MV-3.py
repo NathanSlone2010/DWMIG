6881cace1be07f1875968352f22d79b67efcd468
Nathan Slone <n4llker3nel@protonmail.com>
2026-02-26 06:42:50 +0000

    Update MV-3.py

--- CORE/Modulate-Version/MV-3.py
+++ CORE/Modulate-Version/MV-3.py
@@ -1,4 +1,3 @@
-import time
 print("\n All options: zones [z], sites [s], version [v], history [h], resources [rs], exit")
 while True:
     document = input("\n Input: ")
@@ -9,7 +8,7 @@ while True:
     elif document == "s":
         print("\n Darknet Army Forum: Forum of [edgy] sellers, varying from different things | DigAI: An uncensored AI. Willing to help with any projects | WSS: A Darknet chatroom, full of a different types of people, and featured on videos from people like Tranium | Tor.Link: A site that is a hub for other sites | NEXTCCV.CC: A Darknet market ranging from pre-made scripts, to CCV | CRYPTBB: A forum and a market [FURTHER RESEARCH REQUIRED] | DEF-CON: A legal site that uses onion for protection, where those of Cybersecurity, government cyber jobs, and more come together | DIGITAL THRIFT SHOP (PL): A market selling cheap items on the Darknet ranging from fake IDs to stolen items of medium to high value | GERMANIA: A Germanic-based market selling items [FURTHER RESEARCH REQUIRED] | BLACKMARK: A market selling a high variety of items [FURTHER RESEARCH REQUIRED] | THE HIDDEN WIKI: Not a site on the Darknet but is a wiki-style hub to other sites that are Darknet| Onionindex: Not a site on the Darknet but is a site that leads to other sites that are Darknet [FURTHER RESEARCH REQUIRED] | BREAKING BAD FORUM: A forum and a market place selling items to make the illegal substance Breaking Bad revolves around, also selling recipes alike | Onionlinks: a Darknet hub that links to other Darknet sites | Onion Identity Services: A market that sells fake IDs, fake Passports, and more | Dread: An onion site themed like Reddit, the original went down and now only copy-cats are left | Excavator: A Darknet hub that leads to other sites, even those that should never be interacted with.")
     elif document == "h":
-      print("2.1.1: Cut down the bloat even more, probably made it harder to read the code though | 3.2.1: Cut down bloat. Ability to read the code went skyrocketing down [even cut out some history] | 3.3.1s: Cut down the bloat some more...")
+      print("3.2.1: Cut down bloat. Ability to read the code went skyrocketing down [even cut out some history] | 3.3.1s: Cut down the bloat some more... | 3.4.1: Removed 'import time' as it was not being used.")
     elif document == "rs":
       print("Please refer to Documentation [d] before continuing on this | Tor Browser: https://www.torproject.org/download/ | ProtonVPN: https://protonvpn.com/pricing | MullvadVPN: https://mullvad.net/en/download/vpn/")
     else:
